cde145517be8f0c047c816676b0a9441c1dff404
Guyguy25 <clementtanguy20@gmail.com>
2023-02-22 17:00:52 +0100

    Updates index.js

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-const Discord = require('discord.js');
-const config = require('./config.json');
+const Discord = require("discord.js");
+const config = require("./config.json");
 
-const joincreate = require('./extra/joincreate');
-const welcome = require('./extra/welcome');
-const badwords = require('./extra/forbiddenwords');
-const messageDelete = require('./extra/logs/messageDelete');
-const messageUpdate = require('./extra/logs/messageUpdate');
-const voiceLeave = require('./extra/logs/voiceUpdate');
-const memberCount = require('./extra/memberCount');
-const ticket = require('./extra/ticket')
-const help = require('./extra/helps')
-const birthday = require('./commands/birthday');
+const joincreate = require("./extra/joincreate");
+const welcome = require("./extra/welcome");
+const badwords = require("./extra/forbiddenwords");
+const messageDelete = require("./extra/logs/messageDelete");
+const messageUpdate = require("./extra/logs/messageUpdate");
+const voiceLeave = require("./extra/logs/voiceUpdate");
+const memberCount = require("./extra/memberCount");
+const ticket = require("./extra/ticket");
+const help = require("./extra/helps");
+const birthday = require("./commands/birthday");
 
-fs = require('fs');
+fs = require("fs");
 client = new Discord.Client();
 
 client.setMaxListeners(50);
-// client.login(config.token);
-client.login(process.env.TOKEN);
+client.login(config.token);
 
 client.commands = new Discord.Collection();
 fs.readdir("./commands/", (err, files) => {
-
   if (err) console.log(err);
 
-  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  let jsfile = files.filter((f) => f.split(".").pop() === "js");
 
   if (jsfile.length <= 0) {
-    return console.log('Impossible de trouver des commandes');
+    return console.log("Impossible de trouver des commandes");
   }
 
   jsfile.forEach((f, i) => {
@@ -39,16 +37,16 @@ fs.readdir("./commands/", (err, files) => {
   });
 });
 
-client.on("message", async message => {
+client.on("message", async (message) => {
   if (message.author.bot || message.channel.type === "dm") return;
 
-  let messagearray = message.content.split(" ")
+  let messagearray = message.content.split(" ");
   let cmd = messagearray[0];
   let args = message.content.trim().split(/ +/);
 
   if (!message.content.startsWith(config.prefix)) return;
 
-  let commandfile = client.commands.get(cmd.slice(config.prefix.length))
+  let commandfile = client.commands.get(cmd.slice(config.prefix.length));
 
   if (commandfile) commandfile.run(client, message, args);
 });
@@ -68,58 +66,64 @@ client.on('ready', () => {
   }, 20000);
 });*/
 
-client.on('ready', async (message) => {
-  console.log('LOGS \n\n.-------------------------------.\n|           Commandes           |\n|-------------------------------|');
+client.on("ready", async (message) => {
+  console.log(
+    "LOGS \n\n.-------------------------------.\n|           Commandes           |\n|-------------------------------|"
+  );
   client.user.setActivity("🤍 .help", {
     type: "STREAMING",
     url: "https://www.twitch.tv/guyguy_tv",
-  })
+  });
 
-  joincreate(client)
-  welcome(client)
-  badwords(client)
-  memberCount(client)
-  ticket(client)
-  help(client)
-  birthday(client)
-})
+  joincreate(client);
+  welcome(client);
+  badwords(client);
+  memberCount(client);
+  ticket(client);
+  help(client);
+  birthday(client);
+});
 
 channelID = "910912275521634317";
 
-client.on('voiceStateUpdate', async(oldState, newState) => {
-  voiceLeave(oldState, newState)
+client.on("voiceStateUpdate", async (oldState, newState) => {
+  voiceLeave(oldState, newState);
 
-  if(oldState.channelID === newState.channelID) {
-    console.log('a user has not moved!')
+  if (oldState.channelID === newState.channelID) {
+    console.log("a user has not moved!");
   }
-  if(oldState.channelID != null && newState.channelID != null && newState.channelID != oldState.channelID) {
-      console.log('a user switched channels')
+  if (
+    oldState.channelID != null &&
+    newState.channelID != null &&
+    newState.channelID != oldState.channelID
+  ) {
+    console.log("a user switched channels");
   }
-  if(oldState.channelID === null) {
-      console.log('a user joined!')
+  if (oldState.channelID === null) {
+    console.log("a user joined!");
   }
   if (newState.channelID === null) {
-      console.log('a user left!')
+    console.log("a user left!");
   }
-})
+});
 
-client.on('messageDelete', async(message)=> {
-  messageDelete(message)
-})
+client.on("messageDelete", async (message) => {
+  messageDelete(message);
+});
 
-client.on('messageUpdate', async(oldMessage, newMessage) => {
-  messageUpdate(oldMessage, newMessage)
-})
+client.on("messageUpdate", async (oldMessage, newMessage) => {
+  messageUpdate(oldMessage, newMessage);
+});
 
-function delay(n){
-  return new Promise(function(resolve){
-    setTimeout(resolve,n*1000);
+function delay(n) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, n * 1000);
   });
 }
 
-async function startLOG(){
+async function startLOG() {
   await delay(1.2);
-  console.log('⠂-------------------------------⠂');
+  console.log("⠂-------------------------------⠂");
 }
 
 startLOG();
